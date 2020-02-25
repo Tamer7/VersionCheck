4dbcbaef34a524c11d849ab4edec7fb6d5792bdf
Tamer Jar <tamerjar@hotmail.com>
2020-02-25 23:04:53 +0200

    New Label Texts and Grid changes

--- VersionApp/program.py
+++ VersionApp/program.py
@@ -11,8 +11,8 @@ root = Tk()
 root.title("AppToDate")
 
 # input field to get users email
-user_email = Entry(root, width = 25)
-user_email.pack()
+user_email = Entry(root, width=25)
+user_email.grid(row=3, column=0)
 
 # this runs google chrome webdriver, REPLACE WITH YOUR OWN WEB DRIVER PATH
 driver = webdriver.Chrome("/Users/tamerjar/Desktop/chromedriver")
@@ -21,14 +21,20 @@ driver.get("https://www.python.org/downloads/")
 time.sleep(3)
 
 # This selenium automation gets the latest version of python from the web
-element = driver.find_element(By.XPATH, '// *[ @ id = "content"] / div / section / div[1] / ol / li[1] / span[1] / a')
-chrome_latest_version = element.text
-print("Latest Python Version: " + chrome_latest_version)
+element = driver.find_element(
+    By.XPATH,
+    '// *[ @ id = "content"] / div / section / div[1] / ol / li[1] / span[1] / a',
+)
+python_latest_version = element.text
+print("Latest Python Version: " + python_latest_version)
 
 # This selenium automation gets the latest version of chrome from the web
 driver.get("https://chromedriver.chromium.org/downloads")
 time.sleep(3)
-chrome = driver.find_element(By.XPATH, '//*[@id="sites-canvas-main-content"]/table/tbody/tr/td/div/div[4]/h2/span/a')
+chrome = driver.find_element(
+    By.XPATH,
+    '//*[@id="sites-canvas-main-content"]/table/tbody/tr/td/div/div[4]/h2/span/a',
+)
 final_chrome = chrome.text
 print("Latest Chrome Version: " + final_chrome)
 
@@ -36,7 +42,7 @@ print("Latest Chrome Version: " + final_chrome)
 # This Automation gets latest version of selenium from the web
 driver.get("https://www.selenium.dev/downloads/")
 time.sleep(3)
-sel = driver.find_element(By.XPATH, '/html/body/div[3]/table/tbody/tr[3]/td[2]')
+sel = driver.find_element(By.XPATH, "/html/body/div[3]/table/tbody/tr[3]/td[2]")
 final_sel = sel.text
 print("Latest Selenium Version: " + final_sel)
 
@@ -51,7 +57,7 @@ except:
 
 # code for getting chrome current version on system
 try:
-    chrome_version = driver.capabilities['browserVersion']
+    chrome_version = driver.capabilities["browserVersion"]
     chrome_new = "ChromeDriver " + chrome_version
     # Chrome Version on your system
     print("Your Chrome Version: " + chrome_new)
@@ -68,115 +74,124 @@ except:
 
 
 # ------- THIS CODE DISPLAYS VERSION UPON RUNNING THE APP
-if (chrome_latest_version == finalVersion):
-    frame = LabelFrame(root, padx=25, pady=25)
-    frame.pack()
-    pythonFrame = Label(frame, text= "No Version for python is Available")
+if python_latest_version == finalVersion:
+    frame = LabelFrame(root, padx=15, pady=15)
+    frame.grid(row=5, column=5)
+    pythonFrame = Label(
+        frame, text="You have the Latest Python Version : " + finalVersion
+    )
     pythonFrame.grid(row=0, column=0)
 else:
-    frame = LabelFrame(root, padx=25, pady=25)
-    frame.pack()
-    python_new_version = Label(frame, text = "A New Version for python is Available")
+    frame = LabelFrame(root, padx=15, pady=15)
+    frame.grid(row=5, column=5)
+    python_new_version = Label(
+        frame,
+        text="A New Python version : "
+        + python_latest_version
+        + " is Available for Update",
+    )
     python_new_version.grid(row=0, column=0)
 
 
-
-if (chrome_new == final_chrome):
-    frame = LabelFrame(root, padx=25, pady=25)
-    frame.pack()
-    chromeFrame = Label(frame, text= "No Version for Chrome is Available")
+if chrome_new == final_chrome:
+    frame = LabelFrame(root, padx=15, pady=15)
+    frame.grid(row=5, column=6)
+    chromeFrame = Label(
+        frame, text="You have the Latest Chrome Version : " + chrome_new
+    )
     chromeFrame.grid(row=0, column=0)
 else:
-    frame = LabelFrame(root, padx=25, pady=25)
-    frame.pack()
-    chrome_new_version = Label(frame, text = "A New Version for Chrome is Available")
+    frame = LabelFrame(root, padx=15, pady=15)
+    frame.grid(row=5, column=6)
+    chrome_new_version = Label(
+        frame,
+        text="A New Chrome version : " + final_chrome + " is Available for Update",
+    )
     chrome_new_version.grid(row=0, column=0)
 
 
-if (selenium_computer_version == final_sel):
-    frame = LabelFrame(root, padx=25, pady=25)
-    frame.pack()
-    selFrame = Label(frame, text= "No Version for Selenium is Available")
+if selenium_computer_version == final_sel:
+    frame = LabelFrame(root, padx=15, pady=15)
+    frame.grid(row=5, column=7)
+    selFrame = Label(
+        frame,
+        text="You have the Latest Selenium Version : " + selenium_computer_version,
+    )
     selFrame.grid(row=0, column=0)
 else:
-    frame = LabelFrame(root, padx=25, pady=25)
-    frame.pack()
-    sel_new_Frame = Label(frame, text= "No Version for Selenium is Available")
+    frame = LabelFrame(root, padx=15, pady=15)
+    frame.grid(row=6, column=7)
+    sel_new_Frame = Label(
+        frame, text="A New Selenium version : " + final_sel + " is Available for Update"
+    )
     sel_new_Frame.grid(row=0, column=0)
 
-#------------ ENDS HERE
-
-
+# ------------ ENDS HERE
 
 
 # function to check if new version is available fro python when the button is clicked
 def getPython():
     # checks if new version is available for python
-    if (chrome_latest_version == finalVersion):
-        my_label = Label(root, text = "No Version for python is Available")
-        my_label.pack()
+    if python_latest_version == finalVersion:
+        my_label = Label(root, text="No Version for python is Available")
+        my_label.grid(row=8, column=0)
     else:
-        my_other = Label(root, text = "A New Version for python is Available")
-        my_other.pack()
+        my_other = Label(root, text="A New Version for python is Available")
+        my_other.grid(row=8, column=0)
 
 
 # function to check if new version is available fro chrome when the button is clicked
 def getChrome():
     # checks if new version is available for Chrome
-    if (chrome_new == final_chrome):
-        my_chrome = Label(root, text = "No New Chrome Version Available")
-        my_chrome.pack()
+    if chrome_new == final_chrome:
+        my_chrome = Label(root, text="No New Chrome Version Available")
+        my_chrome.grid(row=10, column=0)
     else:
-        new_chrome = Label(root, text = "A new Chrome Version is Available")
-        new_chrome.pack()
+        new_chrome = Label(root, text="A new Chrome Version is Available")
+        new_chrome.grid(row=10, column=0)
 
 
 # function to check if new version is available fro selenium when the button is clicked
 def getSelenium():
 
-    if (selenium_computer_version == final_sel):
-        my_label_selenium = Label(root, text= "No New Selenium Version is Available")
-        my_label_selenium.pack()
+    if selenium_computer_version == final_sel:
+        my_label_selenium = Label(root, text="No New Selenium Version is Available")
+        my_label_selenium.grid(row=12, column=0)
     else:
-        new_selenium = Label(root, text = "A new Selenium Version is Available")
-        new_selenium.pack()
+        new_selenium = Label(root, text="A new Selenium Version is Available")
+        new_selenium.grid(row=12, column=0)
 
 
 # gets email and stores it in variable email
 def getEmail():
     email = user_email.get()
     if user_email != "":
-        show_message = Label(root, text = "Email Submitted")
-        show_message.pack()
+        show_message = Label(root, text="Email Submitted")
+        show_message.grid(row=3, column=5)
     else:
         print("Empty text field")
     print(email)
 
 
+# button to get email
 
-#button to get email
+email_button = Button(root, text="Submit Email", command=getEmail)
+email_button.grid(row=3, column=4)
 
-email_button = Button(root, text = "Submit Email", command = getEmail)
-email_button.pack()
+# button for python
+my_label_python = Button(root, text="Check Version For Python", command=getPython)
+my_label_python.grid(row=7, column=0)
 
-#button for python
-my_label_python = Button(root, text = "Check Version For Python", command = getPython)
-my_label_python.pack()
+# button for chrome
+my_label_chrome = Button(root, text="Check Version for Chrome", command=getChrome)
+my_label_chrome.grid(row=9, column=0)
 
-#button for chrome
-my_label_chrome = Button(root, text = "Check Version for Chrome", command = getChrome)
-my_label_chrome.pack()
+# button for seleniun
 
-#button for seleniun
-
-my_selenium_button = Button(root, text = "Check Version for Selenium", command = getSelenium)
-my_selenium_button.pack()
-
-
+my_selenium_button = Button(
+    root, text="Check Version for Selenium", command=getSelenium
+)
+my_selenium_button.grid(row=11, column=0)
 
 
 root.mainloop()
-
-
-
-
