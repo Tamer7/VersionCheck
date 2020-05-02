22fe206ab5ac1400f97167de31ba49743377c558
thisiskcn <56170785+thisiskcn@users.noreply.github.com>
2020-05-02 14:25:31 +0300

    updated contact_team link

--- VersionApp/program.py
+++ VersionApp/program.py
@@ -587,7 +587,7 @@ class FindVersion:
 
     def contact_team(self):
         "Link to our Website for contact"
-        webbrowser.open("https://hg49zozjfvzdfaltk2xita-on.drv.tw/www.vum-ds-app2date.online/")
+        webbrowser.open("https://hg49zozjfvzdfaltk2xita-on.drv.tw/www.vum-ds-app2date.online/#footer")
 
 
 root = Tk()
